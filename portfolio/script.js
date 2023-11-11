186d843f713a87488c67718ad8e67abc74e7c7c2
sickpoitew <109254578+Poitew@users.noreply.github.com>
2023-11-11 14:09:15 +0100

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,77 @@
+// CURSOR AURA
+// create a div for the aura and personalize it
+const aura = document.createElement("div");
+
+aura.style.position = "fixed";
+aura.style.width = "50px";
+aura.style.height = "50px";
+aura.style.borderRadius = "50%";
+aura.style.backgroundImage = "radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(92,92,125,1) 50%, rgba(0,28,62,1) 100%)";
+aura.style.opacity = "0.2";
+aura.style.zIndex = "120";
+aura.style.pointerEvents = "none";
+aura.style.transition = "all 0.1s ease-out";
+aura.style.transform = "translate(-50%, -50%)";
+
+// add the aura to the document
+document.body.appendChild(aura);
+
+// listen for mouse movement events
+document.addEventListener("mousemove", (event) => {
+    // update the position of the aura
+    aura.style.left = event.clientX + "px";
+    aura.style.top = event.clientY + "px";
+});
+
+// listen for mouse enter events 
+document.addEventListener("mouseenter", () => {
+    // increase the size of the aura
+    aura.style.width = "100px";
+    aura.style.height = "100px";
+    aura.style.opacity = "0.1";
+});
+
+// listen for mouse leave events
+document.addEventListener("mouseleave", () => {
+    // reset the size of the aura
+    aura.style.width = "50px";
+    aura.style.height = "50px";
+    aura.style.opacity = "0.5";
+});
+
+// listen for scroll events
+document.addEventListener("scroll", () => {
+    // update the position of the aura
+    aura.style.left = event.clientX + "px";
+    aura.style.top = event.clientY + "px";
+});
+
+
+
+// PARAGRAPH CHANGER
+
+// constant for the paragraph
+const homeP = document.querySelector("#nav-home");
+const aboutMeP = document.querySelector("#nav-aboutMe");
+const projectsP = document.querySelector("#nav-projects");
+
+// listener for the scroll events
+window.addEventListener("scroll", () => {
+    const websitePosition = window.scrollY;
+    // change the color of the nav bar paragraph depending on where you are on the website
+    if(websitePosition <= 650){
+        homeP.style.color = "white";
+        aboutMeP.style.color = "darkgray";
+        projectsP.style.color = "darkgray";
+    }
+    else if(websitePosition >= 650 && websitePosition <= 1200){
+        homeP.style.color = "darkgray";
+        aboutMeP.style.color = "white";
+        projectsP.style.color = "darkgray";
+    }
+    else {
+        homeP.style.color = "darkgray";
+        aboutMeP.style.color = "darkgray";
+        projectsP.style.color = "white";
+    }
+})
